4cd02a095bcaecbe6f396c76bbb63c5e257e32d5
tparzadka97 <parzadkatomasz@yahoo.com>
2025-01-03 15:29:15 -0500

    updating navbar

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 
 import "./Navbar.css";
 
 function AppNavbar() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <Navbar expand="sm">
+    <Navbar expand="sm" expanded={expanded} onToggle={setExpanded}>
       <Container>
         <Navbar.Brand as={Link} to="/">
           Rate the Portfolio
@@ -19,16 +21,38 @@ function AppNavbar() {
             </Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Item className="me-auto">
-              <Nav.Link as={Link} to="/login" className="login-btn">
-                Login
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item className="me-auto">
-              <Nav.Link as={Link} to="/signup" className="signup-btn">
-                Signup
-              </Nav.Link>
-            </Nav.Item>
+            {expanded ? (
+              <>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/login">
+                    Login
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/signup">
+                    Signup
+                  </Nav.Link>
+                </Nav.Item>
+              </>
+            ) : (
+              <>
+                <Nav.Item>
+                  <Button
+                    as={Link}
+                    to="/login"
+                    variant="outline-dark"
+                    className="me-2"
+                  >
+                    Login
+                  </Button>
+                </Nav.Item>
+                <Nav.Item>
+                  <Button as={Link} to="/signup" variant="dark">
+                    Signup
+                  </Button>
+                </Nav.Item>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
